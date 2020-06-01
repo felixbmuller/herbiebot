54bd6a52d75b0c1bf3b5dd95b9b8a77a11ccd5af
Felix <felix.mueller98@gmx.net>
2020-06-01 14:37:18 +0200

    doc: added comments

--- herbiebot.py
+++ herbiebot.py
@@ -13,12 +13,15 @@ import argparse
 logger = logging.getLogger(__name__)
 
 def start(update, context):
+    """ hook for /start """
     context.bot.send_message(chat_id=update.effective_chat.id, text=(
         "Hi, I'm HerbieBot. I will save every picture that is sent to me on "
         "the server I am running on. Please send pictures as documents to "
         "avoid loss of quality."))
 
 def extract_file_id(msg):
+    """ `msg` is a message containing either a document or a picture. Extract 
+         the file_id to download and some other relevant information """
     was_document = True
     file_id_to_download = None
     file_ending = None
@@ -27,6 +30,7 @@ def extract_file_id(msg):
         file_id_to_download = msg.document.file_id
         file_ending = mimetypes.guess_extension(msg.document.mime_type)
     elif msg.photo:
+        # photo messages contain the photos in multiple resolutions -> use the best
         max_width = -1
         for photo in msg.photo:
             if photo.width > max_width:
@@ -37,6 +41,7 @@ def extract_file_id(msg):
     if file_ending is None:
         file_ending = ".jpg"
 
+    # extract sender user name for naming the file later
     if msg.forward_from is not None:
         sender = msg.forward_from.username
     elif msg.from_user is not None:
@@ -51,19 +56,23 @@ def extract_file_id(msg):
 
 def download_and_save_file(bot, file_id, save_dir, source_username, file_ending):
     """
-    param is file ending with dot!
+    Download the file and name it properly. This tries to extract the datetime from
+    the exif metadata if possible to use it for the filename. 
+    
+    param is the file ending with dot, e.g. ".jpg"!
     """
 
     # can do max 20MB -> no videos
     file = bot.get_file(file_id)
 
-    # avoid collisions
+    # save with random name first
     random_name = secrets.token_urlsafe()
 
     full_path = os.path.join(save_dir, random_name)
 
     file.download(custom_path=full_path)
 
+    # extract metadate to find a proper name
     with open(full_path, 'rb') as fh:
         tags = exifread.process_file(fh, stop_tag="EXIF DateTimeOriginal")
         dateTaken : str= str(tags.get("EXIF DateTimeOriginal", ""))
@@ -81,12 +90,13 @@ def download_and_save_file(bot, file_id, save_dir, source_username, file_ending)
 
     logger.info(f"Saved file as {new_path}")
 
-    # small race condition, but thats okay 
+    # small race condition here, but thats okay 
     os.rename(full_path, new_path)
 
 SAVE_DIR = ""
 
 def handle_files(update, context):
+    """ hook for receiving a message with a photo or document """
     msg = update.message
 
     try:
@@ -107,11 +117,11 @@ def handle_files(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
 def handle_video(update, context):
+    """ hook for receiving a video """
     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I cannot handle videos.")
 
 def main():
     global SAVE_DIR
-
 
     parser = argparse.ArgumentParser(description='Run HerbieBot to save pictures from Telegram.')
     parser.add_argument('save_dir', help='where to save the images')
